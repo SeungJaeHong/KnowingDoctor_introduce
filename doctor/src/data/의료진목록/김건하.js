9663c8938d6f4a9479d4c069aefaee781e2f990d
배연준 <49051494+yjbae3974@users.noreply.github.com>
2021-11-29 17:24:02 +0900

    파일 정리 완료. 의사1명 추가.

---
+++ doctor/src/data/의료진목록/김건하.js
@@ -0,0 +1,215 @@
+const 김건하 =[
+    {
+        id: 2,
+        프로필: 'https://www.kcch.re.kr/upload/deptdoctors/1572244365500_C18CC544CCADC18CB144ACFC_AE40AC74D558ACFCC7A5.jpg',
+        name: ['김건하','Kim_Kun_Ha'],
+        진료과: '소아청소년과',
+        전문진료분야: ['발달지연','ADHD','두통','어지럼증','경련','소아 호흡기 질환','알레르기 질환','소아 신경 질환'],
+        소속병원: '원자력병원',
+        경력: [
+            {
+                id: 0,
+                시작: '1997.03',
+                끝: '2003.02',
+                내용: '고려대학교 의과대학 졸업',
+            },
+            {
+                id: 1,
+                시작: '2004.03',
+                끝: '2008.02',
+                내용: '고려대학교 병원 소아청소년과 전공의'
+            },
+            {
+                id: 2,
+                시작: '',
+                끝: '2009.05',
+                내용: 'ECGMG(미국의사수련자격)취득'
+            },
+            {
+                id: 3,
+                시작: '',
+                끝: '2011.07',
+                내용: '국제 모유수유전문가(IBCLC)자격취득   '
+            },
+            {
+                id: 4,
+                시작: '2011.03',
+                끝: '2013.02',
+                내용: '고려대학교 병원 소아신경 임상 강사'
+            },
+            {
+                id: 5,
+                시작: '',
+                끝: '2014.02',
+                내용: '고려대학교 의과대학 의학박사 취득'
+            },
+            {
+                id: 6,
+                시작: '2014.03',
+                끝: '2015.02',
+                내용: 'Florida hospital form children 연수'
+            },
+            {
+                id: 7,
+                시작: '2013.03',
+                끝: '2019.02',
+                내용: '고려대학교 소아신경 임상 조교수'
+            },
+            {
+                id: 8,
+                시작: '2019',
+                끝: '',
+                내용: '원자력병원 소아청소년과 과장'
+            }
+        ],
+        수상경력: [
+            {
+                id: 0,
+                날짜: '2018.09',
+                내용: '2018 대한소아신경학회 젊은 연구자상',
+            }
+        ],
+        학회활동: [
+            {
+                id: 0,
+                시작: '2004',
+                끝: '',
+                내용: '대한 소아과학회 회원',
+            },
+            {
+                id: 1,
+                시작: '2011',
+                끝: '',
+                내용: '대한 소아신경학회 회원',
+            },
+            {
+                id: 2,
+                시작: '2011',
+                끝: '',
+                내용: '대한 뇌전증학회 회원',
+            },
+            {
+                id: 3,
+                시작: '2011',
+                끝: '',
+                내용: '대한 두통학회 회원',
+            },
+            {
+                id: 4,
+                시작: '2015',
+                끝: '',
+                내용: 'Asian and Ocean Congress of Child Neurology 평생회원',
+            },
+            {
+                id: 5,
+                시작: '2016',
+                끝: '2018',
+                내용: '2016-2017 대한 소아신경학회 보험위원회 위원',
+            },
+            {
+                id: 6,
+                시작: '2020',
+                끝: '',
+                내용: 'BMC pediatrics editorial board member',
+            },
+            {
+                id: 0,
+                시작: '',
+                끝: '',
+                내용: '대한소아소화기영양학회 정회원',
+            },
+        ],
+        논문: [
+            {
+                분류: '2012',
+                제목: 'Analysis of Interictal Epileptiform Discharges in the Benign Childhood Epilepsy with Centrotemporal Spikes: Prediction of Seizure Outcome.',
+            },
+            {
+                분류: '2012',
+                제목: 'The Comparison of the Ocular Compression Test and the Tilt-table Test in the Suspected Patient of Vasovagal Syncope in Children and Adolescence.',
+            },
+            {
+                분류: '2012',
+                제목: 'Attention deficit hyperactivity disorder in epileptic children.'
+            },
+            {
+                분류: '2013',
+                제목: 'Mycoplasma pneumoniae associated stroke in a 3-year-old girl.',
+            },
+            {
+                분류: '2013',
+                제목: 'Saethre&#8211;Chotzen syndrome with an atypical phenotype: identification of TWIST microdeletion by array CGH.',
+            },
+            {
+                분류: '2014',
+                제목: 'Vitamin D levels in children and adolescents with antiepileptic drug treatment.',
+            },
+            {
+                분류: '2014',
+                제목: 'Clinical Comparison among Patients with Enteroviral Meningitis According to the Presence of CSF Pleocytosis.'
+            },
+            {
+                분류: '2014',
+                제목: 'Charcot-Marie-Tooth disease masquerading as acute demyelinating encephalomyelitis-like illness.',
+            },
+            {
+                분류: '2015',
+                제목: 'Parents’ Subjective Assessment of Effects of Antiepileptic Drug Discontinuation.'
+            },
+            {
+                분류: '2016',
+                제목: 'The role of executive functioning in quality of life in pediatric intractable epilepsy.'
+            },
+            {
+                분류: '2016',
+                제목: 'Norovirus in benign convulsions with mild gastroenteritis.'
+            },
+            {
+                분류: '2016',
+                제목: 'Single-center experience of the Korean-Developmental Screening Test for infants and children.'
+            },
+            {
+                분류: '2017',
+                제목: 'Impact of intraoperative 3-T MRI with diffusion tensor imaging on hemispherectomy.'
+            },
+            {
+                분류: '2017',
+                제목: 'Usefulness of intraoperative insular electrocorticography in modified functional hemispherectomy.'
+            },
+            {
+                분류: '2017',
+                제목: 'Utility of Follow-up Diffusion Tensor Imaging in Acute Hemorrhagic Leukoencephalitis: a Case Report.'
+            },
+            {
+                분류: '2017',
+                제목: 'Neuroprotective Effect of Lacosamide on Hypoxic-Ischemic Brain Injury in Neonatal Rats.'
+            },
+            {
+                분류: '2017',
+                제목: 'Usefulness of the Korean Developmental Screening Test for infants and children for the evaluation of developmental delay in Korean infants and children: a single-center study.'
+            },
+            {
+                분류: '2017',
+                제목: 'Prognosis in Late-Onset Febrile Seizure.'
+            },
+            {
+                분류: '2018',
+                제목:'Prevalence, incidence, and recurrence of febrile seizures in Korean children based on national registry data.'
+            },
+            {
+                분류: '2018',
+                제목: 'Need for lumbar puncture in children younger than 12 months presenting with simple febrile seizure.'
+            },
+            {
+                분류: '2018',
+                제목: 'Association of Low Serum Ionized Magnesium Level with Fever-Triggered Seizures in Epileptic Children.'
+            },
+            {
+                분류: '2021',
+                제목: 'Gut Bacterial Dysbiosis in Children with Intractable Epilepsy'
+            }
+        ]
+    }
+]
+
+export default 김건하
---
+++ doctor/src/data/의료진목록/김은선.js
@@ -0,0 +1,78 @@
+const 김은선 = [
+    {
+        id: 0,
+        프로필: 'http://anam.kumc.or.kr/upload/deptdoctors/1495177597995_AE40C740C120.png',
+        name: ['김은선','Kim_Eun_Sun'],
+        진료과: '소화기내과',
+        전문진료분야: ['위장관 질환','위암','대장암','염증성 장질환'],
+        소속병원: '고려대학교 안암병원',
+        경력: [
+            {
+                id: 0,
+                시작: "1997.03",
+                끝: '2003.02',
+                내용: "고려대학교 의학과 졸업"
+            },
+            {
+                id: 1,
+                시작: "2005.03",
+                끝: '2007.02',
+                내용: "고려대학교 의학과 석사 학위 취득"
+            },
+            {
+                id: 2,
+                시작: "2008.03",
+                끝: '2010.02',
+                내용: "고려대학교 의학과 박사 학위 취득"
+            },
+
+        ],
+        수상경력: [
+            {
+                id: 0,
+                날짜: '2008.12',
+                내용: '대한내과학회 우수 논문상 수상'
+            },
+            {
+                id: 1,
+                날짜: '2009.12',
+                내용: '유럽소화기학회 (UEGW) travel grant award 2개 부문 수상'
+            }
+        ],
+        논문: [
+            {
+                분류: '국외논문',
+                제목: '위장관 스텐트(Gastrointestinal and biliary stents)'
+            },
+            {
+                분류: '국외논문',
+                제목: '대장내시경검사에서 온수와 오일의 유용성(Usefulness of Warm Water and Oil Assistance in Colonoscopy by Trainees)'
+            },
+            {
+                분류: '국외논문',
+                제목: '모자용종증 환자에서 infliximab 투약 후 호전된 증례(Remission of cappolyposis maintained for more than three years after infliximab treatment)'
+            },
+            {
+                분류: '국외논문',
+                제목: '내시경적 치료로 호전된 식도 근층 박리증(Intramural esophageal dissection resolved by endoscopic treatment)'
+            },
+            {
+                분류: '국외논문',
+                제목: '글루코오스의 이광자 현미경 추적자(A Two-Photon Tracer for Glucose Uptake)'
+            },
+            {
+                분류: '국외논문',
+                제목: '위내시경검사 중 페퍼민트오일이 연동운동에 미치는 영향 평가(The Effect of Peppermint Oil on Peristalsis during Gastroscopy)'
+            },
+            {
+                분류: '국내논문',
+                제목: '만성 간질환 환자에서 간 섬유화 평가에 대한 간 탄력도 측정의 유용성(The efficacy of measuring liver stiffness for the assessment of liver fibrosis in patients with chronic liver diseases)'
+            },
+            {
+                분류: '국내논문',
+                제목: 'Atypical Radiological Manifestation of Pulmonary Metastatic Calcification'
+            }
+        ]
+    }
+]
+export default 김은선;
