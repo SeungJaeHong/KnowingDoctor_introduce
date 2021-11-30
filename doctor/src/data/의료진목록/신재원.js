951ead3037eb7ae5eb25c5ec11bd41fc18e96133
yjbae3974 <49051494+yjbae3974@users.noreply.github.com>
2021-11-30 16:32:40 +0900

    .

--- doctor/src/data/의료진목록/신재원.js
+++ doctor/src/data/의료진목록/신재원.js
@@ -30,15 +30,36 @@ const 신재원 =[
                 id: 3,
                 시작: '2007',
                 끝: '2011',
-                내용: '고려대학교 대학원 의학과 박사',
+                내용: '고려대학교 구로병원 외과 전공의',
             },
+            {
+                id: 4,
+                시작: '2011',
+                끝: '2013',
+                내용: '고려대학교 안암병원 대장항문외과 임상강사',
+            },
+            {
+                id: 5,
+                시작: '',
+                끝: '',
+                내용: '대한대장항문외과 세부 전문의 취득',
+            },
+            {
+                id: 6,
+                시작: '',
+                끝: '',
+                내용: '대한대장항문학회 인증 대장내시경 전문의 취득',
+            },
+
+
+
 
         ],
         수상경력: [
             {
                 id: 0,
-                날짜: '',
-                내용: '',
+                날짜: '2018',
+                내용: '대한대장항문학회 우수포스터 학술상 수상',
             }
         ],
         학회활동: [
@@ -46,13 +67,37 @@ const 신재원 =[
                 id: 0,
                 시작: '',
                 끝: '',
-                내용: '',
+                내용: '대한외과학회 평생회원',
+            },
+            {
+                id: 1,
+                시작: '',
+                끝: '',
+                내용: '대한대장항문학회 평생회원',
             }
         ],
+        논문개수: 14,
         논문: [
+            
             {
-                분류: '',
-                제목: '',
+                분류: '2018',
+                제목: '제 44차 항문질환연구회 집담회 발표 (Hemorrhoidectomy in my clinic)',
+            },
+            {
+                분류: '2018',
+                제목: '춘계대장항문학회 poster (An analysis of surgical outcomes for rectal prolapsed at a single institution)'
+            },
+            {
+                분류: '2018',
+                제목: 'Septic condition after hemorrhoidectomy'
+            },
+            {
+                분류: '2019',
+                제목: 'pilonidal sinus의 수술적 치료'
+            },
+            {
+                분류:'2019',
+                제목: '춘계 대장항문학회 구연 (Surgical outcomes of Sphincter-Preserving Procedure for Suprasphincteric Fistula using Submucosal Approach Flap with Excision of fistula tract (SAFE))'
             }
         ]
     }
