a1cbcbd58bc3467f45fa24573477d6edd28dade2
배연준 <49051494+yjbae3974@users.noreply.github.com>
2021-12-08 11:53:05 +0900

    의사 2명 데이터베이스 추가

---
+++ doctor/src/data/의료진목록/박병호.js
@@ -0,0 +1,114 @@
+const 박병호 =[
+    {
+        id: 0,
+        프로필: 'https://cdn.kmpnews.co.kr/news/photo/201607/21300_12875_3532.jpg',
+        name: ['박병호','Park_Byeong_Ho'],
+        진료과: '성형외과',
+        URL: 'http://www.eyehops.com/mobile/index.php',
+        전문진료분야: ['눈성형','눈재수술','눈매교정'],
+        소속병원: '아이호성형외과',
+        학력: [
+            {
+                id: 0,
+                내용: '고려대학교 의과대학 졸업'
+            },
+        ],
+        경력: [
+            {
+                id: 0,
+                내용: '고려대학교 안암병원 수련의',
+            },
+            {
+                id: 1,
+                내용: '송파특전사 사령부 군의관'
+            },
+            {
+                id: 2,
+                내용: '분당차병원성형외과 전문의 과정'
+            },
+            {
+                id: 3,
+                내용: '분당차병원성형외과 외래교수'
+            },
+            {
+                id: 4,
+                내용: '(전)쥬얼리 성형외과 원장'
+            },
+            {
+                id: 5,
+                내용: '(전) JYP 성형외과 대표원장'
+            },
+            {
+                id: 6,
+                내용: '(전)첫눈 성형외과 공동원장'
+            },
+            {
+                id: 7,
+                내용: '(전)원진 성형외과 원장'
+            },
+            {
+                id: 8,
+                내용: '(전)대한성형외과의사회 간사'
+            },
+            {
+                id: 9,
+                내용: '(현) 연성대학교 학교법인 감사'
+            },
+            {
+                id: 10,
+                내용: '(현) 아이호 성형외과 대표원장'
+            }
+        ],
+        학회활동: [
+            {
+                id: 0,
+                내용: '대한성형외과학회 종신회원',
+            },
+            {
+                id: 1,
+                내용: '대한미용성형외과학회 정회원'
+            },
+            {
+                id: 2,
+                내용: '대한성형외과의사회 정회원'
+            },
+            {
+                id: 3,
+                내용: '대한두개악안면성형외과학회 정회원'
+            },
+            {
+                id: 4,
+                내용: 'Member of the International Confederation for Plastic, Reconstructive and Aesthetic surgery'
+            },
+            {
+                id: 5,
+                내용: 'Member of Asian Section of IPRAS'
+            }
+        ],
+        논문개수: '6',
+        논문: [
+            {
+                분류: '대한성형외과학회 기초재건 포럼 발표',
+                제목: 'Treatment of Fromatal Sinus Fracture Using Bio-absorbable Mesh Plate',
+            },
+            {
+                분류: '대한성형외과학회 학술대회 발표',
+                제목: 'Effect of B16 Melanoma Conditioned Medium on Fibroliferative Scar Formation'
+            },
+            {
+                분류: 'SCI',
+                제목: 'Fingertip Reconstruction With Simultaneous Flaps and Nail Bad Grafts Following Amputation'
+            },
+            {
+                분류: 'KCI',
+                제목: 'Traumatic Oculomotor Nerve Palsy'
+            },
+            {
+                분류: 'KCI',
+                제목: 'Primary Intraosseous Hemangioma is Frontal Bone'
+            }
+        ]
+    }
+]
+
+export default 박병호;
