56ff297e3596d521bcc7a99dc42f25fd3f7b2f31
배연준 <49051494+yjbae3974@users.noreply.github.com>
2021-12-09 16:01:38 +0900

    의사 3명 db 추가

---
+++ doctor/src/data/의료진목록/박선자.js
@@ -0,0 +1,40 @@
+const 박선자 =[
+    {
+        id: 0,
+        프로필: 'https://www.kosinmed.or.kr/images/dept/doctor_data/GI_PSJ180.jpg',
+        name: ['박선자','Park_Seon_Ja'],
+        진료과: '소화기내과',
+        URL: 'https://www.kosinmed.or.kr/03dept/dept01.htm?mode=do_list&i_no=11',
+        전문진료분야: ['궤양','물혹','암','염증','신경','대장암 클리닉'],
+        소속병원: '고신대학교 복음병원',
+        학력: [
+            {
+                id: 0,
+                내용: '부산대학교 의과대학 졸업'
+            },
+            {
+                내용: '인제대학교 의학대학원 박사 학위 취득'
+            }
+        ],
+        경력: [
+            {
+                id: 0,
+                내용: '내과 전문의 취득',
+            },
+            {
+                내용: '소화기내과 교수'
+            }
+        ],
+
+        학회활동: [
+            {
+                id: 0,
+
+                내용: '미국 MD Anderson Cancer Center 연수',
+            }
+        ],
+
+    }
+]
+
+export default 박선자;
---
+++ doctor/src/data/의료진목록/박승혁.js
@@ -0,0 +1,38 @@
+const 박승혁 =[
+    {
+        id: 0,
+        프로필: 'https://lh3.googleusercontent.com/proxy/kwXqe9ojwdk5ACuONch8ODSy2cVGgFWH7FvHzj7jWbj77U3DtIdfgzufOuSY8fQ1bTk_j-STgZfB73y96obxDSxHfBYSp9hRhoULxi5oYWF4TyOER1Ofx0J2VJFt_EjBhPE23ozm1q49vMGdulLyGYUv9EWjWAvub1lCUyLh-085uOVLWD_Wob2VHUnllwt31UTU',
+        name: ['박승혁','Park_Seung_Hyeok'],
+        진료과: '소화기내과',
+        URL: 'http://cwd8th.com/html_2013/01/02.php',
+        전문진료분야: ['위.대장클리닉','성장클리닉'],
+        소속병원: '춘원당한의원',
+        학력: [
+            {
+                id: 0,
+                내용: '경희대학교 한의과대학 졸업'
+            },
+            {
+                id: 1,
+                내용: '경희대학교 대학원 임상한의학 석사, 박사 학위 취득'
+            }
+        ],
+        경력: [
+            {
+                내용: '경희대학교 한방병원 전문의 취득',
+            },
+            {
+                내용: '경희대학교 한방병원 임상조교수 역임',
+            }
+        ],
+        학회활동: [
+            {
+                id: 0,
+                
+                내용: '대한한의과학회 정회원',
+            }
+        ],
+    }
+]
+
+export default 박승혁;
